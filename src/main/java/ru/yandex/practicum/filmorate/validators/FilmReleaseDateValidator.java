3af71d743a5fd62f16cacd660962ad2f1187f257
Rusty <leo59158@yandex.ru>
2025-06-25 12:09:52 +0500

    Класс для проверки даты релиза фильма на валидность

--- src/main/java/ru/yandex/practicum/filmorate/validators/FilmReleaseDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validators/FilmReleaseDateValidator.java
@@ -1,3 +1,5 @@
+package ru.yandex.practicum.filmorate.validators;
+
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import ru.yandex.practicum.filmorate.annotation.ValidReleaseDate;
@@ -5,13 +7,13 @@ import ru.yandex.practicum.filmorate.annotation.ValidReleaseDate;
 import java.time.LocalDate;
 
 public class FilmReleaseDateValidator implements ConstraintValidator<ValidReleaseDate, LocalDate> {
-    private static final LocalDate FIRST_FILM_DATE = LocalDate.of(1895, 12, 28);
+    private static final LocalDate MIN_FILM_DATE = LocalDate.of(1895, 12, 28);
 
     @Override
     public boolean isValid(LocalDate value, ConstraintValidatorContext context) {
         if (value == null) {
-            return true; // null обрабатывается @NotNull
+            return true;
         }
-        return !value.isBefore(FIRST_FILM_DATE);
+        return !value.isBefore(MIN_FILM_DATE);
     }
 }
