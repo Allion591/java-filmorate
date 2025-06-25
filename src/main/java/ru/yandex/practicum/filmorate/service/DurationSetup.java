df0588e2705dcd9f00ffd76f5f85f5b7d0dfbcd2
Rusty <leo59158@yandex.ru>
2025-06-25 12:08:40 +0500

    Реализован класс для сериализации и десериализации продолжительности

--- src/main/java/ru/yandex/practicum/filmorate/service/DurationSetup.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/DurationSetup.java
@@ -11,21 +11,18 @@ import java.io.IOException;
 import java.time.Duration;
 
 public class DurationSetup {
-    public class DurationDeserializer extends JsonDeserializer<Duration> {
+    public static class DurationDeserializer extends JsonDeserializer<Duration> {
         @Override
         public Duration deserialize(JsonParser p, DeserializationContext ctx) throws IOException {
-            long seconds = p.getLongValue(); // Получаем число из JSON
-            return Duration.ofSeconds(seconds); // Создаем Duration из секунд
+            long minutes = p.getLongValue();
+            return Duration.ofMinutes(minutes);
         }
     }
 
-    // Сериализатор: Duration -> число (секунды)
-    public class DurationSerializer extends JsonSerializer<Duration> {
+    public static class DurationSerializer extends JsonSerializer<Duration> {
         @Override
         public void serialize(Duration value, JsonGenerator gen, SerializerProvider provider) throws IOException {
-            gen.writeNumber(value.getSeconds()); // Преобразуем Duration в секунды (число)
+            gen.writeNumber(value.toMinutes());
         }
     }
-
 }
-
