a0c13b20dc00c27b191ce4917c2e888a45dc14bd
Rusty <leo59158@yandex.ru>
2025-07-09 16:06:23 +0500

    Реализован класс для хранения фильмов а так же методы для работы с фильмами

--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
@@ -1,4 +1,80 @@
 package ru.yandex.practicum.filmorate.storage;
 
-public class InMemoryFilmStorage {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.interfaces.FilmStorage;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Slf4j
+@Component
+public class InMemoryFilmStorage implements FilmStorage {
+    private final Map<Long, Film> films = new HashMap<>();
+    private final AtomicLong nextId = new AtomicLong(1);
+
+    @Override
+    public Film create(Film newFilm) {
+        log.info("Получена команда на добавление фильма {}", newFilm);
+        log.info("Фильм успешно прошел валидацию");
+        newFilm.setId(getNextId());
+
+        films.put(newFilm.getId(), newFilm);
+        log.info("Фильм успешно сохранен");
+        return newFilm;
+    }
+
+    private long getNextId() {
+        long newId = nextId.getAndIncrement();
+        log.info("Генерация id для фильма: {}", newId);
+        return newId;
+    }
+
+    @Override
+    public Film update(Film film) {
+        log.info("Получена команда на обновление фильма {}", film);
+
+        log.info("Фильм успешно прошел валидацию для обновления");
+        if (films.containsKey(film.getId())) {
+            Film oldFilm = films.get(film.getId());
+            log.info("Фильм найден в памяти");
+
+            oldFilm.setName(film.getName());
+            oldFilm.setDescription(film.getDescription());
+            oldFilm.setReleaseDate(film.getReleaseDate());
+            oldFilm.setDuration(film.getDuration());
+            log.info("Все поля успешно изменены {}", oldFilm);
+            return oldFilm;
+        }
+        log.warn("Фильм c данным id каталоге не найден {}", film.getId());
+        throw new NotFoundException("Фильм с названием = " + film.getName() + " не найден");
+    }
+
+    @Override
+    public void delete(Film film) {
+        if (films.containsKey(film.getId())) {
+            films.remove(film.getId());
+        } else {
+            throw new NotFoundException("Фильм не найден");
+        }
+    }
+
+    @Override
+    public Collection<Film> findAll() {
+        log.info("Получена команда на получение всех фильмов");
+        return films.values();
+    }
+
+    @Override
+    public Film getFilmById(Long id) {
+        if (films.containsKey(id)) {
+            return films.get(id);
+        } else {
+            throw new NotFoundException("Фильм не найден");
+        }
+    }
 }
