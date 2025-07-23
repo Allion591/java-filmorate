331dafd4cee8f35d3904bf3c4d544922d29d91a9
Rusty Talgaty <leo59158@gmail.com>
2025-07-23 15:21:50 +0500

    Реализован класс для хранения данных жанров

--- src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
@@ -1,4 +1,13 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@ToString
+@EqualsAndHashCode(of = "id")
 public class Genre {
+    private final long id;
+    private final String name;
 }
