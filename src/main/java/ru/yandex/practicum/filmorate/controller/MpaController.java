09b6a0e0aa6a285beb498ed77375ee65f5bd9688
Allion591 <leo59158@yandex.ru>
2025-08-09 15:30:32 +0500

    Добавлен новый контроллер для обработки рейтингов

--- src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
@@ -1,4 +1,30 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.model.Mpa;
+import ru.yandex.practicum.filmorate.service.FilmService;
+
+import java.util.Collection;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/mpa")
 public class MpaController {
+    private final FilmService filmService;
+
+    @GetMapping(value = {"/{id}"})
+    public ResponseEntity<Mpa> getMpaNameById(@PathVariable Long id) {
+        return new ResponseEntity<>(filmService.getMpaNameById(id), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<Collection<Mpa>> mpaGetAll() {
+        return new ResponseEntity<>(filmService.mpaGetAll(), HttpStatus.OK);
+    }
 }
