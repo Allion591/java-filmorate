d1382155fafc978d77f89bbcc690c1c2a8669d03
Allion591 <leo59158@yandex.ru>
2025-08-09 15:34:01 +0500

    Реализован интерфей GenreRepository для работы с классом БД

--- src/main/java/ru/yandex/practicum/filmorate/interfaces/GenreRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/interfaces/GenreRepository.java
@@ -1,4 +1,24 @@
 package ru.yandex.practicum.filmorate.interfaces;
 
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Genre;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.Set;
+
 public interface GenreRepository {
+
+    public Genre findById(Long genreId);
+
+    public Collection<Genre> findAll();
+
+    public void validateGenres(Set<Genre> genre);
+
+    public void addGenreFromResultSet(ResultSet rs, Film film) throws SQLException;
+
+    public void saveFilmGenres(Film film);
+
+    public void updateFilmGenres(Film film);
 }
