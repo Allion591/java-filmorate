03ddd6cadb206cd7a79b4010227ec2a64d567028
Allion591 <leo59158@yandex.ru>
2025-08-09 16:50:29 +0500

    Добавлен конструктор ручной для сборки проекта

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,7 +1,9 @@
 package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.lang.NonNull;
 
 import java.time.LocalDate;
@@ -9,6 +11,8 @@ import java.util.Set;
 import java.util.TreeSet;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private Long id;
     private Set<Long> friends = new TreeSet<>();
@@ -29,4 +33,10 @@ public class User {
     @NotNull(message = "Дата рождения обязательна")
     @PastOrPresent(message = "Дата рождения не может быть в будущем")
     private LocalDate birthday;
+
+    public User(String email, String login, LocalDate birthday) {
+        this.email = email;
+        this.login = login;
+        this.birthday = birthday;
+    }
 }
