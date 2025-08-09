9ce18516568fe9a692e073a08b01884afb2224ec
Allion591 <leo59158@yandex.ru>
2025-08-09 15:51:35 +0500

    Адаптированы тесты для контроллеров с учетом внесенных изменений в приложение

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.filmorate;
+package ru.yandex.practicum.filmorate.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,7 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.transaction.annotation.Transactional;
 import ru.yandex.practicum.filmorate.model.Film;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import ru.yandex.practicum.filmorate.model.Genre;
