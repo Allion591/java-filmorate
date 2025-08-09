b3b5b2298fd7d07487b4a4211d2e74d321f97339
Allion591 <leo59158@yandex.ru>
2025-08-09 15:32:36 +0500

    Реализован интерфей для работы с классом БД

--- src/main/java/ru/yandex/practicum/filmorate/interfaces/FilmRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/interfaces/FilmRepository.java
@@ -4,15 +4,18 @@ import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.Collection;
 
-public interface FilmStorage {
+public interface FilmRepository {
 
-    public Film create(Film newFilm);
 
     public Film update(Film film);
 
     public Collection<Film> findAll();
 
-    public void delete(Film film);
-
     public Film getFilmById(Long id);
+
+    public Film save(Film film);
+
+    public void deleteById(Long id);
+
+    public Collection<Film> findPopularFilms(Long count);
 }
