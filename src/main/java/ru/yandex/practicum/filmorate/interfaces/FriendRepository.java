eccd49f565647e16417d8e9be288531095efc282
Allion591 <leo59158@yandex.ru>
2025-08-09 15:33:26 +0500

    Реализован интерфей FriendRepository для работы с классом БД

--- src/main/java/ru/yandex/practicum/filmorate/interfaces/FriendRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/interfaces/FriendRepository.java
@@ -1,4 +1,20 @@
 package ru.yandex.practicum.filmorate.interfaces;
 
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.List;
+
 public interface FriendRepository {
+
+    public void addFriend(long userId, long friendId);
+
+    public void removeFriend(long userId, long friendId);
+
+    public void updateFriends(User user);
+
+    public boolean existsById(long userId);
+
+    public void loadFriendsForUser(User user);
+
+    public List<User> findFriendsByUserId(long userId);
 }
