f452fbfc825f57b51c0f901bb1cb5fae9a12a3cf
Allion591 <leo59158@yandex.ru>
2025-08-09 15:44:56 +0500

    Реализовван класс для работы с базой данных хранящии рейтинги

--- src/main/java/ru/yandex/practicum/filmorate/repository/JdbcMpaRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/JdbcMpaRepository.java
@@ -1,4 +1,49 @@
 package ru.yandex.practicum.filmorate.repository;
 
-public class JdbcMpaRepository {
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.interfaces.MpaRepository;
+import ru.yandex.practicum.filmorate.model.Mpa;
+
+import java.util.Collection;
+
+@Slf4j
+@Repository
+@Qualifier("jdbcMpaRepository")
+@RequiredArgsConstructor
+public class JdbcMpaRepository implements MpaRepository {
+    private final NamedParameterJdbcOperations jdbcOperations;
+
+    public Mpa findById(Long mpaId) {
+        String sql = "SELECT * FROM mpa WHERE mpa_id = :mpa_id";
+        try {
+            log.info("Запрос в базу {}", mpaId);
+            return jdbcOperations.queryForObject(
+                    sql,
+                    new MapSqlParameterSource("mpa_id", mpaId),
+                    (rs, rowNum) -> new Mpa(
+                            rs.getLong("mpa_id"),
+                            rs.getString("mpa_name"))
+
+            );
+        } catch (EmptyResultDataAccessException e) {
+            log.info("Ошибка {}", e.getMessage());
+            throw new NotFoundException("MPA рейтинг не найден");
+        }
+    }
+
+    public Collection<Mpa> mpaGetAll() {
+        String sql = "SELECT * FROM mpa GROUP BY mpa_id";
+        return jdbcOperations.query(
+                sql, (rs, rowNum) -> new Mpa(
+                        rs.getInt("mpa_id"),
+                        rs.getString("mpa_name")
+                ));
+    }
 }
