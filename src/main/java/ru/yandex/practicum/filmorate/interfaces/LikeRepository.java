13b9808946d86feb538c8f25f8d68274c656bc26
Allion591 <leo59158@yandex.ru>
2025-08-09 15:35:07 +0500

    Реализован интерфей LikeRepository для работы с классом БД

--- src/main/java/ru/yandex/practicum/filmorate/interfaces/LikeRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/interfaces/LikeRepository.java
@@ -1,4 +1,15 @@
 package ru.yandex.practicum.filmorate.interfaces;
 
+import ru.yandex.practicum.filmorate.model.Like;
+
+import java.util.List;
+
 public interface LikeRepository {
+    public List<Like> findLikesByFilmId(Long filmId);
+
+    public void addLike(Long filmId, Long userId);
+
+    public void removeLike(Long filmId, Long userId);
+
+    public Long getLikesCountForFilm(Long filmId);
 }
